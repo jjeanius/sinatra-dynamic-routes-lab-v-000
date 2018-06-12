e6439b635a3f96fa190ae47020045e1a82814773
Judy McGoldrick <jjeanius@verizon.net>
2018-06-12 03:56:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-  case operation
+  case @operation
     when 'add'
       (@number1 + @number2).to_s
     when 'subtract'
