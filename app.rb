b82c7abb4dac6c79bc026051de0e8eef4055c8f6
Judy McGoldrick <jjeanius@verizon.net>
2018-06-12 00:48:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
+  get '/reversename/:name' do
+    erb :name||:name.reverse
+  end
+
+  get '/squre/:number' do
+  "#{params[:number].to_i*.sqrt}"
+  end
 end
