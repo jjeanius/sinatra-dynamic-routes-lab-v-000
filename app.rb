80df3b67b00452bb0cde5488f37613a28c938aa5
Judy McGoldrick <jjeanius@verizon.net>
2018-06-12 02:20:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,12 +4,10 @@ class App < Sinatra::Base
 
   get '/reversename/:name' do
     params[:name].reverse
-
-
   end
 
   get '/square/:number' do
-  "#{params[sqrt(:number)].to_i}"
+    params[sqrt(:number)].to_i}
   end
 
   get '/say/:number/:phrase' do
