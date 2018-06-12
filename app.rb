5d067fbd36b41f15ec12533d559fe46f0e9882d3
Judy McGoldrick <jjeanius@verizon.net>
2018-06-12 01:06:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,3 +10,17 @@ class App < Sinatra::Base
   "#{params[sqrt(:number)].to_i}"
   end
 end
+
+  get '/say/:number/:phrase' do
+
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+  end
+
+  get '/:operation/:number1/:number2' do
+
+  end
+
+  end
